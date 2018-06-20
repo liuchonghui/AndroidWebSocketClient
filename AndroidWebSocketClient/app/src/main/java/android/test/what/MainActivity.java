9ae8be65f2b67ef4a5fd88c1e210ab216de1131e
liuchonghui <liuchonghui@xiaomi.com>
2018-06-20 10:48:32 +0800

    commit changes.
    N/A
    
    Signed-off-by: liuchonghui <liuchonghui@xiaomi.com>

---
+++ AndroidWebSocketClient/app/src/main/java/android/test/what/MainActivity.java
@@ -0,0 +1,48 @@
+package android.test.what;
+
+import android.annotation.TargetApi;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mHandler = new Handler(new HandlerThread("client-what-single-thread") {{
+            start();
+        }}.getLooper());
+
+        final TextView hint1 = (TextView) findViewById(R.id.hint1);
+        final Button btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        final TextView hint2 = (TextView) findViewById(R.id.hint2);
+        final Button btn2 = (Button) findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        final TextView hint3 = (TextView) findViewById(R.id.hint3);
+        final Button btn3 = (Button) findViewById(R.id.btn3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+            @Override
+            public void onClick(View v) {
+            }
+        });
+    }
+}
